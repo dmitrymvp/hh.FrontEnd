8953a63a681baef02d4acd45f772c27517eaa189
DmitryMVP <masqueunclub17@gmail.com>
2025-08-25 15:14:40 +0500

    5.2.5

--- src/main.tsx
+++ src/main.tsx
@@ -11,7 +11,7 @@ const store = setupStore();
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
-      <HashRouter basename="/hh.FrontEnd/">
+      <HashRouter>
         <App />
       </HashRouter>
     </Provider>
